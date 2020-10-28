cf5aefcfbd162ab86b5f75a4cc736d34fb549455
佐藤　聖利 <ohayoupoteto20000202@ezweb.ne.jp>
2020-10-28 20:17:06 +0900

    ユーザー一覧の表示

--- 202010/20201026_vue/script.js
+++ 202010/20201026_vue/script.js
@@ -1,5 +1,50 @@
+const getUsers = function (callback) {
+    setTimeout(function () {
+        callback(null, [
+            {
+                id: 1,
+                name: "ゆんゆん"
+            },
+            {
+                id: 2,
+                name: "六花ちゃん"
+            }
+        ]);
+    }, 100);
+};
+
+
 const UserList = {
-    template: "#user-list"
+    template: "#user-list",
+    data: function () {
+        return {
+            loading: false,
+            users: function () {
+                return [];
+            },
+            error: null
+        };
+    },
+    created: function () {
+        this.fetchData();
+    },
+    watch: {
+        "$route": "fetchData"
+    },
+    methods: {
+        fetchData: function () {
+            this.loading = true;
+            getUsers((function (err, users) {
+                this.loading = false;
+                if (err) {
+                    this.error = err.toString();
+                }
+                else {
+                    this.users = users;
+                }
+            }).bind(this));
+        }
+    }
 };
 
 const router = new VueRouter({
