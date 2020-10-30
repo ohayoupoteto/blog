eb2610866f10c73175b201a757bf3adc7877b7e0
佐藤　聖利 <ohayoupoteto20000202@ezweb.ne.jp>
2020-10-30 18:37:01 +0900

    ユーザー追加機能の実装

--- 202010/20201026_vue/script.js
+++ 202010/20201026_vue/script.js
@@ -1,19 +1,48 @@
+const userData = [
+    {
+        id: 1,
+        name: "ゆんゆん",
+        description: "ボンキュッボン"
+    },
+    {
+        id: 2,
+        name: "六花ちゃん",
+        description: "太もも。"
+    }
+];
+
 const getUsers = function (callback) {
     setTimeout(function () {
         callback(null, [
             {
                 id: 1,
-                name: "ゆんゆん"
+                name: "ゆんゆん",
             },
             {
                 id: 2,
-                name: "六花ちゃん"
+                name: "六花ちゃん",
             }
         ]);
     }, 100);
 };
+const getUser = function (userId, callback) {
+    setTimeout(function () {
+        let filteredUsers = userData.filter(function (user) {
+            return user.id == parseInt(userId, 10);
+        });
+        callback(null, filteredUsers && filteredUsers[0])
+    }, 1000);
+};
+const postUser = function (params, callback) {
+    setTimeout(function () {
+        params.id = userData.length + 1;
+        userData.push(params);
+        callback(null, params);
+    }, 1000);
+};
 
 
+//ユーザー一覧のコンポーネント
 const UserList = {
     template: "#user-list",
     data: function () {
@@ -47,6 +76,84 @@ const UserList = {
     }
 };
 
+//ユーザー詳細のコンポーネント
+const UserDetail = {
+    template: "#user-detail",
+    data: function () {
+        return {
+            loading: false,
+            user: null,
+            error: null
+        };
+    },
+    created: function () {
+        this.fetchData();
+    },
+    watch: {
+        "$route": "fetchData"
+    },
+    methods: {
+        fetchData: function () {
+            this.loading = true;
+            getUser(this.$route.params.userId, (function (err, user) {
+                this.loading = false;
+                if (err) {
+                    this.error = err.toString();
+                }
+                else {
+                    this.user = user
+                }
+            }).bind(this))
+        }
+    }
+};
+//ユーザー登録コンポーネント
+const UserCreate = {
+    template: "#user-create",
+    data: function () {
+        return {
+            sending: false,
+            user: this.defaultUser(),
+            error: null
+        }
+    },
+    created: function () {
+    },
+    methods: {
+        defaultUser: function () {
+            return {
+                name: "",
+                description: ""
+            }
+        },
+        createUser: function () {
+            //バリデーション
+            if (this.user.name.trim() === "") {
+                this.error = "名前は必須です";
+                return;
+            }
+            if (this.user.description.trim() === "") {
+                this.error = "Descriptionは必須です";
+                return;
+            }
+            postUser(this.user, (function (err, user) {
+                this.sending = false;
+                if (err) {
+                    this.error = err.toString;
+                }
+                else {
+                    this.error = null;
+                    this.user = this.defaultUser();
+                    alert("新規ユーザーが登録されました");
+                    //ユーザー一覧ページに戻る
+                    this.$router.push("/users");
+                }
+            }).bind(this));
+        }
+    }
+}
+
+//ルーティング
 const router = new VueRouter({
     routes: [
         {
@@ -58,6 +165,14 @@ const router = new VueRouter({
         {
             path: "/users",
             component: UserList
+        },
+        {
+            path: "/users/new",
+            component: UserCreate
+        },
+        {
+            path: "/users/:userId",
+            component: UserDetail
         }
     ]
 });
